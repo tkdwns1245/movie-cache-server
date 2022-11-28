76e3128ccb7ae497a1eb10b75bb5ead5519bf60c
SANGJUNSON <tkdwns1245@gmail.com>
2022-11-28 20:25:53 +0900

    concat movie data with image source

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ require('isomorphic-fetch');
 const server = express();
 const appCache = new nodecache({ stdTTL : 3599});
 const todosURL = 'https://jsonplaceholder.typicode.com/todos';
+let dailyBoxOfficeList;
 server.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
   });
 server.get('/', async (req,res) => {
-    if(appCache.has('movieData')){
+    if(appCache.has('movieDataList')){
         console.log('Get data from Node Cache');
-        return res.json(appCache.get('movieData'))
+        return res.json(appCache.get('movieDataList'))
     }
 })
 const movieParams = {
@@ -26,27 +27,31 @@ server.listen(4000, (err) => {
     if (err) return console.log(err);
     axios.get('http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',{ params: movieParams })
       .then((Response)=>{
-        appCache.set("movieData",Response.data)
+        
           return Response.data;
       }).then((response) =>{
-        let dailyBoxOfficeList = response.boxOfficeResult.dailyBoxOfficeList;
+        dailyBoxOfficeList = response.boxOfficeResult.dailyBoxOfficeList;
+        
         let searchParams;
         const naverHeader ={
           'X-Naver-Client-Id' : 'hjOsmn7IHqbI4ljQ1m8X',
           'X-Naver-Client-Secret' : 'kdx99NEa9k',
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
         }
         for(var i = 0; i < dailyBoxOfficeList.length; i++){
-          // searchParams = {query:dailyBoxOfficeList[i].movieNm}
-          // axios.get('/v1/search/movie.json',{
-          //   params:searchParams,
-          //   headers:naverHeader,
-          //   withCredentials: true,
-          //   credentials: 'same-origin',
-          // }).then((movieData)=> 
-          // {
-          // })
+          searchParams = {query:dailyBoxOfficeList[i].movieNm}
+          axios.get('https://openapi.naver.com/v1/search/movie.json',{
+            params:searchParams,
+            headers:naverHeader,
+          }).then((movieData)=> 
+          {
+            for(let i = 0; i < dailyBoxOfficeList.length; i++){
+                if(movieData.data.items[0].title.includes(dailyBoxOfficeList[i].movieNm)){
+                    dailyBoxOfficeList[i].image = movieData.data.items[0].image;
+                }    
+            }
+            console.log(dailyBoxOfficeList);
+            appCache.set("movieDataList",dailyBoxOfficeList);
+          })
         } 
       }).catch((Error) => {
           console.log(Error);
